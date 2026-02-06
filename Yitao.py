5f804f6f13a557f6ba244e2fd125115047dfc443
yitaozzz123 <yitaozzz123@gmail.com>
2026-02-06 13:02:14 +0100

    nearest neighbour stuff

--- Yitao.py
+++ Yitao.py
@@ -1,10 +1,11 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
+nDim = 3
 sigma = 3e-10
 epsilon = 1
 
-def lennardJones(r):
+
 
 # returns force from LJ potential with rVec: target -> interacting particle
 def singleInteractionForce(rVec):           # rVec is the seperation from target particle to surrounding particles
@@ -13,6 +14,20 @@ def singleInteractionForce(rVec):           # rVec is the seperation from target
     return F
     # single interaction force. For total, sum up all interactions
 
-def 
+# takes a list of vectors from target to interacting particles, returns total force
+def totalInteractionForce(rs):
+    totalForce = np.zeros(nDim)
+    for i in range(len(rs)):
+        totalForce += singleInteractionForce(rs[i])
+    return totalForce
+
+# Finds nearest copy for minimum interaction convention
+def MICneighbour(rVec):
+    return np.mod(rVec + 0.5*L*np.ones(nDim), np.ones(nDim)) - 0.5*L*np.ones(nDim)
+    # returns the conversion to nearest periodic clone
+
+
+
+
 
 
