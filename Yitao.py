a213e6f1d87608e47ea814d5bf9f448dba8fe74f
yitaozzz123 <yitaozzz123@gmail.com>
2026-02-06 12:31:59 +0100

    LJ2

--- Yitao.py
+++ Yitao.py
@@ -6,10 +6,13 @@ epsilon = 1
 
 def lennardJones(r):
 
-def singleInteractionForce(rVec):
-    r = np.sqrt(np.dot(rVec, rVec))
+# returns force from LJ potential with rVec: target -> interacting particle
+def singleInteractionForce(rVec):           # rVec is the seperation from target particle to surrounding particles
+    r = np.sqrt(np.dot(rVec, rVec))         # norm of rVec
     F = epsilon*(48*(sigma**12)*(r**-14) - 24*(sigma**6)*(r**-8))*rVec
     return F
+    # single interaction force. For total, sum up all interactions
 
+def 
 
 
