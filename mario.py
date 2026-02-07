d6dbd7698b6268dc108479cc9f0a74c93e35fd1b
Mario Maalini <magalinimario@gmail.com>
2026-02-07 19:49:52 +0100

    fix on the tail animation and plot for the particles the travel across borders

--- mario.py
+++ mario.py
@@ -6,43 +6,59 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from collections import deque
 
-
+#Numerical setup
 box_size=10
 n_dim=2
 num_particles=100
 mass=0.1
 timestep=0.01
+tail_lenght=6
+num_iterations=100
 
-
+#initialization of position, velocity 
 rss = np.zeros((num_particles, n_dim))
 pos=np.random.uniform(0, box_size, size=(num_particles, n_dim) )
 vel=np.random.uniform(-10, 10, size=(num_particles, n_dim) )
-tail=deque(maxlen=6)
+
+#tail initialization
+tail=deque(maxlen=tail_lenght)
 tail.append(pos.copy())
 
-for i in range(1000):
+for i in range(num_iterations):
 
-    #computation of the force
-    F=np.zeros((num_particles, n_dim))
-
+    #computation of the force and acceleration
+    F=np.zeros((num_particles, n_dim))    
     acceleration=F/mass
+
+    #update of positions and velocity for every interaction
     pos+=vel*timestep
     vel+=acceleration*timestep
 
+    #application of the periodic boundary conditions
     pos[pos>box_size] -= box_size
     pos[pos<0] += box_size
 
+    #tail update
     tail.append(pos.copy())
 
-    hist = np.stack(tail, axis=0)
+    #from deque to numpy to have a functioning plot
+    tail_numpy = np.stack(tail, axis=0)
 
     plt.clf()
 
-    if len(tail)==6 and n_dim==2:
-        plt.scatter(pos[:,0],pos[:,1],marker='o')
+    #we do not want to plot the tail of a particle if the periodic boundary condition happened 
+    d = np.diff(tail_numpy, axis=0)                          
+    wrapped = np.any(np.abs(d) > box_size/2, axis=2) 
+    yes_tail_index = ~np.any(wrapped, axis=0)                
 
-        for p in range(num_particles):
-            plt.plot(hist[:,p,0],hist[:,p,1])
+
+    if len(tail)==tail_lenght and n_dim==2:
+        plt.scatter(pos[:,0],pos[:,1],marker='o')
+            
+        plottable_tail=tail_numpy[:,yes_tail_index,:]
+        
+        for plottable_particle in range(np.shape(plottable_tail)[1]):
+            plt.plot(plottable_tail[:,plottable_particle,0],plottable_tail[:,plottable_particle,1])
     
         plt.xlim(0, box_size)
         plt.ylim(0, box_size)
